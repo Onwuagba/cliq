504275e107aec99a36bc24683320e3da4aebb54a
Kenenna Onwuagba <onwuagbakenenna@gmail.com>
2024-08-15 18:42:11 +0100

    update: add HMAC check for form data (used in image generation)

--- common/middleware.py
+++ common/middleware.py
@@ -2,7 +2,8 @@ import hashlib
 import hmac
 import json
 from datetime import datetime
-import time, logging
+import time
+import logging
 
 from django.conf import settings
 
@@ -54,7 +55,8 @@ class AutoLogoutMiddleware:
                 # last_activity comes back as str so I convert to datetime
                 date_string = last_activity
                 date_format = "%Y-%m-%d %H:%M:%S.%f%z"
-                datetime_obj = datetime.strptime(date_string, date_format)
+                datetime_obj = datetime.strptime(
+                    date_string, date_format)
 
                 inactive_duration = timezone.now() - datetime_obj
                 if inactive_duration.total_seconds() > 300:
@@ -68,24 +70,31 @@ class AutoLogoutMiddleware:
 
 class ChecksumMiddleware(MiddlewareMixin):
     # security middleware to secure communication and ensure data integrity
-    def compute_hmac(self, data, key):
+    def compute_hmac(self, request, key):
         """
         Compute the HMAC (Hash-based Message Authentication Code) of the given data using the provided key.
 
         Parameters:
-            data (bytes, str, dict): The data to be hashed. It can be a bytes object, a string, or a dictionary.
+            request: HTTPRequest Object.
             key (bytes): The secret key used for HMAC computation.
 
         Returns:
             str: The computed HMAC as a hexadecimal string, or None if the data type is not supported.
         """
-        if isinstance(data, bytes):
+        data = request.body
+        if request.content_type == 'multipart/form-data':
+            # For multipart form data, use only the POST data for HMAC
+            # for image upload in qr generation
+            data = json.dumps(request.POST.dict(), sort_keys=True).encode("utf-8")
+            return hmac.new(key, data, hashlib.sha256).hexdigest()
+        elif isinstance(data, bytes):
             return hmac.new(key, data, hashlib.sha256).hexdigest()
         elif isinstance(data, str):
             return hmac.new(key, data.encode("utf-8"), hashlib.sha256).hexdigest()
         elif isinstance(data, dict):
             return hmac.new(
-                key, json.dumps(data, sort_keys=True).encode("utf-8"), hashlib.sha256
+                key, json.dumps(data, sort_keys=True).encode(
+                    "utf-8"), hashlib.sha256
             ).hexdigest()
         return None
 
@@ -104,7 +113,9 @@ class ChecksumMiddleware(MiddlewareMixin):
         """
         if request.method in ["POST", "PUT", "PATCH"]:
             try:
-                payload = request.body
+                if not request.body or request.path.startswith("/admin/"):
+                    # qr code generation is a POST request & may not have a body
+                    return
                 received_hmac = request.META.get("HTTP_X_HMAC")
                 if not received_hmac:
                     return JsonResponse(
@@ -116,7 +127,7 @@ class ChecksumMiddleware(MiddlewareMixin):
                     )
 
                 secret_key = settings.SECRET_KEY.encode("utf-8")
-                computed_hmac = self.compute_hmac(payload, secret_key)
+                computed_hmac = self.compute_hmac(request, secret_key)
 
                 if received_hmac != computed_hmac:
                     return JsonResponse(
@@ -171,13 +182,15 @@ class JWTCookieMiddleware:
             jwt_authenticator = JWTAuthentication()
             try:
                 # raw_token = access_token.encode(HTTP_HEADER_ENCODING)
-                validated_token = jwt_authenticator.get_validated_token(access_token)
+                validated_token = jwt_authenticator.get_validated_token(
+                    access_token)
                 exp_time = validated_token["exp"]
                 current_time = int(time.time())
 
                 # Check if token is about to expire in 1 minute
                 if exp_time - current_time < 60 and refresh_token:
-                    new_access_token = self.refresh_access_token(refresh_token)
+                    new_access_token = self.refresh_access_token(
+                        refresh_token)
                     if new_access_token:
                         self.set_jwt_cookie(request, new_access_token)
                         request.META["HTTP_AUTHORIZATION"] = (
@@ -186,10 +199,12 @@ class JWTCookieMiddleware:
 
                 # Update request authorization header with the access token
                 # request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
-                request.user = jwt_authenticator.get_user(validated_token)
+                request.user = jwt_authenticator.get_user(
+                    validated_token)
             except TokenError:
                 if refresh_token:
-                    new_access_token = self.refresh_access_token(refresh_token)
+                    new_access_token = self.refresh_access_token(
+                        refresh_token)
                     if new_access_token:
                         self.set_jwt_cookie(request, new_access_token)
                         request.META["HTTP_AUTHORIZATION"] = (
@@ -198,11 +213,13 @@ class JWTCookieMiddleware:
                         validated_token = jwt_authenticator.get_validated_token(
                             new_access_token
                         )
-                        request.user = jwt_authenticator.get_user(validated_token)
+                        request.user = jwt_authenticator.get_user(
+                            validated_token)
                 else:
                     request.user = None
             except Exception as e:
-                logger.error(f"Error from JWT middleware: {str(e.args[0])}")
+                logger.error(f"Error from JWT middleware: {
+                             str(e.args[0])}")
                 request.user = None
         else:
             request.user = None
@@ -218,7 +235,8 @@ class JWTCookieMiddleware:
             return None
 
     def set_jwt_cookie(self, response, token):
-        cookie_max_age = settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"].total_seconds()
+        cookie_max_age = settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"].total_seconds(
+        )
         response.set_cookie(
             key=settings.SIMPLE_JWT["AUTH_COOKIE"],
             value=token,
